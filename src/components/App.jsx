4bca17a8fa38c6f46cf670477963947db5ff2c17
Yukhnenko Nataliia <101944838+Natali2721@users.noreply.github.com>
2022-10-20 17:40:44 +0300

    profile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,19 @@
+import { Profile } from '../components/profile/profile';
+import user from '../data/userData.json';
+import css from '../common/App.module.css';
+
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div className={css.main}>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+        key={user.tag}       
+      />
     </div>
   );
 };
